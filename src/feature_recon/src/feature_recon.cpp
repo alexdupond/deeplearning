4d513a94fa8ca1be77bd71941a8963c227603e90
alexdupond <alexdupond@live.dk>
2018-10-12 14:43:14 +0200

    Made helper class for data

--- src/feature_recon/src/feature_recon.cpp
+++ src/feature_recon/src/feature_recon.cpp
@@ -5,10 +5,29 @@
 #include "feature_recon/BodyPartElm.h"
 #include <sstream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
-void callback(const feature_recon::Persons::ConstPtr& msg){
+class FeatureExtractor
+{
+public:
+  FeatureExtractor(ros::NodeHandle &node_handle);
+
+  void callback(const feature_recon::Persons::ConstPtr& msg);
+  double calDistance(feature_recon::BodyPartElm first, feature_recon::BodyPartElm secound);
+  bool isBodyPair(int id1, int id2);
+
+private:
+  ros::NodeHandle nh;
+  ros::Subscriber extract_features_sub;
+};
+
+FeatureExtractor::FeatureExtractor(ros::NodeHandle &node_handle): nh (node_handle){
+  extract_features_sub =  nh.subscribe("broadcaster/poses", 1, &FeatureExtractor::callback, this);
+}
+
+void FeatureExtractor::callback(const feature_recon::Persons::ConstPtr& msg){
   if(msg->persons.size()){
     if(msg->persons[0].body_part.size()){
       for (int i = 0; i < msg->persons[0].body_part.size(); i++) {
@@ -20,15 +39,39 @@ void callback(const feature_recon::Persons::ConstPtr& msg){
   }
 }
 
+double FeatureExtractor::calDistance(feature_recon::BodyPartElm first, feature_recon::BodyPartElm second){
+  return sqrt(pow((second.x-first.x), 2) + pow((second.y-first.y), 2) + pow((second.z-first.z),2));
+}
+
+bool FeatureExtractor::isBodyPair(int id1, int id2){
+  int idPAirs[18][5] = {{1, 14, 15, -1, -1},
+                        {0, 2, 5, 8, 11},
+                        {1, 3, -1, -1, -1},
+                        {2, 4, -1, -1, -1},
+                        {3, -1, -1, -1, -1},
+                        {1, 6, -1, -1, -1},
+                        {5, 7, -1, -1, -1},
+                        {6, -1, -1, -1, -1},
+                        {1, 9, -1, -1, -1},
+                        {8, 10, -1, -1, -1},
+                        {9, -1, -1, -1, -1},
+                        {1, 12, -1, -1, -1},
+                        {11, 13, -1, -1, -1},
+                        {12, -1, -1, -1 ,-1},
+                        {0, 16, -1, -1, -1},
+                        {0, 17, -1, -1, -1},
+                        {14, -1, -1, -1, -1},
+                        {15, -1, -1, -1, -1}}
+}
+
 
 int main(int argc, char **argv)
 {
 
 ros::init(argc, argv, "deature_recon");
 
-ros::NodeHandle n;
-
-ros::Subscriber get_features = n.subscribe("broadcaster/poses", 1, callback);
+ros::NodeHandle nh;
+FeatureExtractor FeatExt(nh);
 
 ros::Rate loop_rate(10);
 
