39a3038d1329ee1d3945f8d12f2d172be6cd897a
Quist <chni215@student.sdu.dk>
2018-11-29 21:57:13 +0100

    confidence function added

--- src/feature_recon/src/feature_recon.cpp
+++ src/feature_recon/src/feature_recon.cpp
@@ -46,3 +46,53 @@ while (ros::ok()){
 
 return 0;
 }
+
+
+double comfidenceFunction(human_data &h1, human_data &h2)
+{
+    int minLimbs = 6;
+
+    int h1Size = h1.limbs.size();
+    int h2Size = h2.limbs.size();
+
+    double length = 0.0;
+    double confidence = 0.0;
+
+    int comparedLimbs = 0;
+
+    if (h1Size <= h2Size && h1Size >= minLimbs)
+    {
+        for (size_t i = 0; i < h1Size; i++)
+        {
+            for (size_t j = 0; j < h2Size; j++)
+            {
+                if(h1.limbs[i].id == h2.limbs[j].id)
+                {
+                    length += abs(h1.limbs[i].avg_info.length - h2.limbs[j].avg_info.length);
+                    confidence += (h1.limbs[i].avg_info.joint_confidence + h2.limbs[j].avg_info.joint_confidence);
+                    comparedLimbs++;
+                }
+            }
+        }
+        if (comparedLimbs >= minLimbs)
+          return (length/h1Size)*(confidence/(h1Size*2));
+    }
+    else if (h2Size < h1Size && h2Size >= minLimbs)
+    {
+        for (size_t i = 0; i < h2Size; i++)
+        {
+            for (size_t j = 0; j < h1Size; j++)
+            {
+                if(h1.limbs[i].id == h2.limbs[j].id)
+                {
+                    length += (h2.limbs[i].avg_info.length / h1.limbs[j].avg_info.length);
+                    confidence += (h2.limbs[i].avg_info.joint_confidence + h1.limbs[j].avg_info.joint_confidence);
+                    comparedLimbs++;
+                }
+            }
+        }
+        if (comparedLimbs >= minLimbs)
+          return (length/h2Size)*(confidence/(h2Size*2));
+    }
+    return -1;
+}
